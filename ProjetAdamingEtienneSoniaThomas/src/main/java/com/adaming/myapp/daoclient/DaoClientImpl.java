6e660decbec45dde336926dae020d0d3c26cc04d
inti0218 <inti0218@DESKTOP-M1VKQFG>
2016-11-22 21:46:09 +0100

    methodes CRUD

--- ProjetAdamingEtienneSoniaThomas/src/main/java/com/adaming/myapp/daoclient/DaoClientImpl.java
+++ ProjetAdamingEtienneSoniaThomas/src/main/java/com/adaming/myapp/daoclient/DaoClientImpl.java
@@ -7,6 +7,40 @@
 
 package com.adaming.myapp.daoclient;
 
+import java.util.List;
+
+import com.adaming.myapp.entities.Client;
+
 public class DaoClientImpl extends AbstractDaoClientJPA implements IDaoClient {
+
+	@Override
+	public Client add(Client entity) {
+		// TODO Auto-generated method stub
+		return add(entity);
+	}
+
+	@Override
+	public Client getOne(Long id) {
+		// TODO Auto-generated method stub
+		return getOne(id);
+	}
+
+	@Override
+	public List<Client> getAll() {
+		// TODO Auto-generated method stub
+		return getAll();
+	}
+
+	@Override
+	public Client update(Client entity) {
+		// TODO Auto-generated method stub
+		return update(entity);
+	}
+
+	@Override
+	public Client delete(Long id) {
+		// TODO Auto-generated method stub
+		return delete(id);
+	}
 
 }
