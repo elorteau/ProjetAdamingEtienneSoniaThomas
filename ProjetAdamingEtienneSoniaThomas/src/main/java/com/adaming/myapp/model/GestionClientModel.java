88f4195d6294bc6d6f6aad0baa52081284eca95a
elorteau <etienne.lorteau@gmail.com>
2016-11-25 12:14:01 +0100

    E > create Model and Controller

---
+++ ProjetAdamingEtienneSoniaThomas/src/main/java/com/adaming/myapp/model/GestionClientModel.java
@@ -0,0 +1,20 @@
+/*
+ * AbstractDaoEmployeJPA
+ * Version: 1.0.0
+ * Date: 25/11/2016
+ * Author: S. Ferhaten - E. Lorteau - T. Saint Georges
+ */
+
+package com.adaming.myapp.model;
+
+public class GestionClientModel {
+	
+	//=========================
+	// Attributes
+	//=========================
+
+	//=========================
+	// Getter / Setter
+	//=========================
+
+}
