f35bbb518bcd3379ea2a0366d50c10e99ffe2a4c
inti0356 <inti0356@DESKTOP-CKFB1NA>
2016-11-25 13:08:14 +0100

    commit thomas controle model baque + client (add)

--- ProjetAdamingEtienneSoniaThomas/src/main/java/com/adaming/myapp/controller/AddClientController.java
+++ ProjetAdamingEtienneSoniaThomas/src/main/java/com/adaming/myapp/controller/AddClientController.java
@@ -7,14 +7,33 @@
 
 package com.adaming.myapp.controller;
 
+import javax.inject.Inject;
+
+import org.apache.log4j.Logger;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.adaming.myapp.model.AddClientModel;
+import com.adaming.myapp.serviceclient.IServiceClient;
+
 public class AddClientController {
 
 	//=========================
 	// Attributes
 	//=========================
-
+	
+	@Inject
+	private IServiceClient serviceClient;
+	private final Logger LOGGER = Logger.getLogger("clientController");
+	
 	//=========================
 	// Methods
 	//=========================
+	
+	@RequestMapping(value ="/addClient", method = RequestMethod.GET)
+	public String addClient(AddClientModel clientModel){
+		
+		return "/comptesClient";
+	}
 
 }
